063dea445eb64157b6de2c2ae81b27a6340e0111
Алексей <hipaman@bigmir.net>
2024-04-22 18:02:56 +0300

    asd

---
+++ prs/Entity/entity.py
@@ -0,0 +1,65 @@
+from enum import Enum
+from uuid import UUID, uuid4
+
+import pydantic
+
+
+class PlayerChoice(Enum):
+    Rock = "rock"
+    Paper = "paper"
+    Scissors = "scissors"
+
+
+class Player (pydantic.BaseModel):
+    id: UUID = pydantic.Field(default_factory=uuid4)
+    name: pydantic.constr(max_length=20)
+    choice: PlayerChoice | None = None
+    score: int = 0
+
+
+class Room(pydantic.BaseModel):
+    id: UUID
+    name: str
+    players: list[Player] = pydantic.Field(default_factory=lambda: [])
+    required_players: int
+
+    def add_player(self, player: Player) -> None:
+        self.players.append(player)
+
+    def reset(self) -> None:
+        for i in self.players:
+            i.choice = None
+
+    @property
+    def can_start(self) -> bool:
+        return len(self.players) == self.required_players
+
+    @property
+    def all_players_make_choice(self) -> bool:
+        return all(i.choice for i in self.players)    # делаю цикл из всех игроков єтой комнаті и смотрю что бі уних біл сделан вібор
+
+    @property
+    def winners(self) -> list[Player]:
+        rock_players = []
+        paper_players = []
+        scissors_players = []
+
+        for player in self.players:
+            if player.choice == PlayerChoice.Rock:
+                rock_players.append(player)
+            elif player.choice == PlayerChoice.Paper:
+                paper_players.append(player)
+            elif player.choice == PlayerChoice.Scissors:
+                scissors_players.append(player)
+
+        if rock_players and paper_players and scissors_players:
+            return self.players
+
+        elif rock_players and paper_players and not scissors_players:
+            return paper_players
+
+        elif rock_players and not paper_players and scissors_players:
+            return rock_players
+
+        elif not rock_players and paper_players and scissors_players:
+            return scissors_players
---
+++ prs/main.py
@@ -0,0 +1,136 @@
+import asyncio
+import json
+from enum import Enum
+from uuid import UUID, uuid4
+
+import pydantic
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse
+from starlette.websockets import WebSocket, WebSocketDisconnect
+
+from prs.Entity.entity import Room, Player, PlayerChoice
+
+app = FastAPI()
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []  # после : указіваю тип
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket, code: int, reason: str | None = None):
+        self.active_connections.remove(websocket)
+        await websocket.close(code, reason)
+
+    @staticmethod
+    async def send_personal_message(message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+    # async def broadcast(self, message: str):
+    #     for connection in self.active_connections:
+    #         await connection.send_text(message)
+
+
+class RoomsManager: # отвечает за связь комнаты с интерфейсом(апи/методов работы с сервером)
+    def __init__(self):
+        self.rooms: dict[UUID, Room] = {}  #создаю пустое множество(колекцию) комнат с типом Словарь у которого ключ ююайди == ююайди комнаті
+        self.players_and_websocket: dict[UUID, WebSocket] = {}
+
+    def create(self, name: str, req_players: int) -> Room:
+        current_room = Room(id=uuid4(), name=name, required_players=req_players)
+        self.rooms[current_room.id] = current_room
+        return current_room
+
+    def get_room(self, room_id: UUID) -> Room | None:
+        return self.rooms.get(room_id, None)    #get принимает два значения 1 получает по первому если нет то что во втором параметре
+
+    def register_player(self, player: Player, websocket: WebSocket) -> None:
+        self.players_and_websocket[player.id] = websocket
+
+
+room_manager = RoomsManager()  #создание обькта класса идет со скобками
+
+
+@app.post("/create_room")
+async def create_room(name: str, req_players: int) -> Room:
+    new_room: Room = room_manager.create(name, req_players)
+    return new_room
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/start/{room_id}")
+async def websocket_connect_room(websocket: WebSocket, room_id: UUID, name: str):
+    await manager.connect(websocket)
+
+    try:
+        room = room_manager.get_room(room_id)
+        if not room:
+            await manager.disconnect(websocket, 1003, reason="room net")
+            return
+        player = Player(name=name)
+        room.add_player(player)
+        send_room: None = json.dump(room)
+
+        while True:
+            await manager.send_personal_message("WaitPlayers", websocket)
+
+            if room.can_start:  # когда подключилось заданое количество игроков
+                await manager.send_personal_message("Game can be started", websocket)
+                if room.all_players_make_choice:
+                    await manager.send_personal_message("YouGetChoice", websocket) #когда все сделали ход
+                    if player in room.winners:
+                        if len(room.winners) == len(room.players):
+                            await manager.send_personal_message("Draw", websocket)
+                        else:
+                            await manager.send_personal_message("You Win", websocket)
+                            player.score += 1
+                    else:
+                        await manager.send_personal_message("You Lose", websocket)
+
+                    room.reset()
+
+                player_input = await websocket.receive_text()
+                try:
+                    player.choice = PlayerChoice(player_input)
+                except ValueError:
+                    await manager.send_personal_message(f"not valid choice", websocket)
+            await asyncio.sleep(0.3)
+            # await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        await manager.disconnect(websocket, 1003, reason="konec")
+        # await manager.broadcast(f"Client #{client_id} left the chat")
+
+
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket, room_id: UUID | None = None):
+#     await manager.connect(websocket)
+#     try:
+#         # if not room_id:
+#         #     new_room = room_manager.create()
+#         while True:
+#             data = await websocket.receive_text()
+#
+#             # new_room = room_manager.create()
+#             await manager.send_personal_message(f"You wrote: {new_room.id}", websocket)
+#             # await manager.broadcast(f"Client #{client_id} says: {data}")
+#     except WebSocketDisconnect:
+#         manager.disconnect(websocket)
+#         # await manager.broadcast(f"Client #{client_id} left the chat")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=("http://localhost:5173", "http://localhost:8080"),
+    allow_credentials=True,
+    allow_methods=("POST", "GET",),
+    allow_headers=("*",)
+)
+
+if __name__ == '__main__':
+    uvicorn.run("prs.main:app", port=7000, log_level='info')
